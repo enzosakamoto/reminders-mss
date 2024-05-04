262848a5b49b22624cc95d70e89809b4e092e5c4
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-04 11:14:09 -0300

    feat(api): add events endpoint

--- observacoes/src/index.ts
+++ observacoes/src/index.ts
@@ -1,22 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import { v4 as uuid4 } from 'uuid'
-import { api } from './api'
+import { apiReminder, apiEvents } from './api'
+import { config } from 'dotenv'
+import { Observation, Reminder } from './types'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-
-type Observation = {
-  id: string
-  text: string
-  reminderId: string
-}
-
-type Reminder = {
-  id: string
-  text: string
-}
+config()
 
 const observations: Observation[] = []
 
@@ -32,7 +24,7 @@ app.get('/reminders/:id/observations', async (req, res) => {
       message: 'Missing a reminder ID'
     })
 
-  const reminders = await api
+  const reminders = await apiReminder
     .get<Reminder>(`/reminders/${id}`)
     .then((response) => {
       return response.data
@@ -73,7 +65,7 @@ app.post('/reminders/:id/observations', async (req, res) => {
       message: 'Missing text field'
     })
 
-  const reminders = await api
+  const reminders = await apiReminder
     .get<Reminder>(`/reminders/${id}`)
     .then((response) => {
       return response.data
@@ -88,21 +80,98 @@ app.post('/reminders/:id/observations', async (req, res) => {
       message: 'There is no reminders with this ID'
     })
 
-  const observation: Observation = {
+  const newObservation: Observation = {
     id: uuid4(),
     text,
     reminderId: id
   }
 
-  observations.push(observation)
+  try {
+    await apiEvents.post('/events', {
+      type: 'observationCreated',
+      payload: newObservation
+    })
+  } catch (error) {
+    console.error(error)
+  }
 
   res.status(200).json({
-    observation: observation,
-    message: 'The observation was created'
+    observation: newObservation,
+    message: 'The observation is being created'
   })
 })
 
-const PORT = 3000
+app.post('/events', (req, res) => {
+  const event = req.body
+
+  if (!event.type || !event.payload) {
+    return res.status(400).json({
+      message: 'Missing type or payload'
+    })
+  }
+
+  console.log('Received event:', event)
+
+  if (event.type === 'observationCreated') {
+    const { id, text } = event.payload
+    observations.push({ id: uuid4(), text, reminderId: id })
+
+    return res.status(201).json({
+      reminder: event.payload,
+      message: 'The event was received and processed'
+    })
+  }
+
+  res.status(200).json({
+    message: 'The event was received'
+  })
+})
+
+app.delete('/reminders/:id/observations/:observationId', async (req, res) => {
+  const { id, observationId } = req.params
+
+  if (!id)
+    return res.status(400).json({
+      message: 'Missing a reminder ID'
+    })
+
+  if (!observationId)
+    return res.status(400).json({
+      message: 'Missing a observation ID'
+    })
+
+  const reminders = await apiReminder
+    .get<Reminder>(`/reminders/${id}`)
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      console.error(error)
+      return null
+    })
+
+  if (!reminders)
+    return res.status(404).json({
+      message: 'There is no reminders with this ID'
+    })
+
+  const observationIndex = observations.findIndex(
+    (observation) => observation.id === observationId
+  )
+
+  if (observationIndex === -1)
+    return res.status(404).json({
+      message: 'There is no observation with this ID'
+    })
+
+  observations.splice(observationIndex, 1)
+
+  res.status(200).json({
+    message: 'The observation was deleted'
+  })
+})
+
+const { PORT } = process.env || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
